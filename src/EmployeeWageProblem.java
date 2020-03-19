5519a2fec0b7de8d040702cc79e4c8efebb7f5c9
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-19 23:18:44 +0530

    ADDED:Check Employee is Present or Absent

--- src/EmployeeWageProblem.java
+++ src/EmployeeWageProblem.java
@@ -1,5 +1,14 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int IS_PRESENT=1;
+
+        int attendanceCheck= (int) (Math.floor(Math.random()*10)%2);
+        if (attendanceCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else System.out.println("Employee is Absent");
     }
 }
