92999b41dc2feb8db2ab14dcd5b4e06629819079
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-23 11:33:20 +0530

    ADDED:Using Switch Case Calculate Full , Part Time Daily Employee Wage

--- src/com/bl/employeewage/EmployeeWageProblem.java
+++ src/com/bl/employeewage/EmployeeWageProblem.java
@@ -1,28 +1,32 @@
 package com.bl.employeewage;
 
 public class EmployeeWageProblem {
+
+    private static final int IS_FULLTIME = 1;
+    private static final int IS_PARTTIME = 2;
+    private static final int WAGE_PER_HOUR = 20;
+    private static int empHours = 0;
+    private static int salary = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-
-        int WAGE_PER_HOUR = 20;
-        int salary = 0, empHours = 0;
-        int IS_FULLTIME = 1, IS_PARTTIME = 2;
-
         int attendanceCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-        if (attendanceCheck == IS_FULLTIME) {
-            System.out.println("Employee is Full Time");
-            empHours = 8;
-        } else {
-            if (attendanceCheck == IS_PARTTIME) {
+        switch (attendanceCheck) {
+            case IS_FULLTIME:
+                System.out.println("Employee is Full Time");
+                empHours = 8;
+                break;
+            case IS_PARTTIME:
                 System.out.println("Employee is Part Time");
                 empHours = 4;
-            } else {
+                break;
+            default:
                 System.out.println("Employee is Absent");
-            }
+                break;
         }
         salary = empHours * WAGE_PER_HOUR;
-        System.out.println("Employee Salary:=" + salary);
+        System.out.println("Employee Salary:-" + salary);
     }
 }
