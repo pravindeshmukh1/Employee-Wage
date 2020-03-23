ca5c3968dfd458ca6c3512bf3aa8d4007debe2dc
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-23 19:34:36 +0530

    ADDED:Calculate salary of Day and Total of salary of Month

--- src/com/bl/employeewage/EmployeeWageProblem.java
+++ src/com/bl/employeewage/EmployeeWageProblem.java
@@ -2,18 +2,23 @@ package com.bl.employeewage;
 
 public class EmployeeWageProblem {
 
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         //  int totalSalary=0;
         final int IS_FULL_TIME = 1, IS_PART_TIME = 2;
         final int WAGE_PER_HOUR = 20;
         final int workingOfDay = 20;
-        int empHours = 0, salary = 0;
-        int totalSalary = 0;
+        final int maxHours = 100;
 
-        for (int day = 1; day <= workingOfDay; day++) {
+        int totalSalary = 0;
+        int noOfDay = 0, totalEmpHours = 0;
+
+        while (noOfDay < workingOfDay && totalEmpHours < maxHours) {
             int attendanceCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
             System.out.println(attendanceCheck);
+            int empHours = 0, salary = 0;
             switch (attendanceCheck) {
                 case IS_FULL_TIME:
                     System.out.println("Employee is Full Time");
@@ -27,9 +32,11 @@ public class EmployeeWageProblem {
                     System.out.println("Employee is Absent");
                     break;
             }
+            noOfDay += 1;
             salary = empHours * WAGE_PER_HOUR;
-            System.out.println("Employee Salary:-" + salary);
+            totalEmpHours += empHours;
             totalSalary += salary;
+            System.out.println("Employee Salary:-" + salary+" of the Day:-"+noOfDay);
         }
         System.out.println("Employee Salary Of Month:-" + totalSalary);
     }
