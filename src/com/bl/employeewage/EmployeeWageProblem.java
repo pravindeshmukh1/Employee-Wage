8b24e6ca52da2836da5eefcc449600bc68a0a550
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-23 19:09:43 +0530

    ADDED:Calculate Wage of the Month

--- src/com/bl/employeewage/EmployeeWageProblem.java
+++ src/com/bl/employeewage/EmployeeWageProblem.java
@@ -2,31 +2,35 @@ package com.bl.employeewage;
 
 public class EmployeeWageProblem {
 
-    private static final int IS_FULLTIME = 1;
-    private static final int IS_PARTTIME = 2;
-    private static final int WAGE_PER_HOUR = 20;
-    private static int empHours = 0;
-    private static int salary = 0;
-
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        //  int totalSalary=0;
+        final int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+        final int WAGE_PER_HOUR = 20;
+        final int workingOfDay = 20;
+        int empHours = 0, salary = 0;
+        int totalSalary = 0;
 
-        int attendanceCheck = (int) (Math.floor(Math.random() * 10) % 3);
-
-        switch (attendanceCheck) {
-            case IS_FULLTIME:
-                System.out.println("Employee is Full Time");
-                empHours = 8;
-                break;
-            case IS_PARTTIME:
-                System.out.println("Employee is Part Time");
-                empHours = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                break;
+        for (int day = 1; day <= workingOfDay; day++) {
+            int attendanceCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println(attendanceCheck);
+            switch (attendanceCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full Time");
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part Time");
+                    empHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
+            salary = empHours * WAGE_PER_HOUR;
+            System.out.println("Employee Salary:-" + salary);
+            totalSalary += salary;
         }
-        salary = empHours * WAGE_PER_HOUR;
-        System.out.println("Employee Salary:-" + salary);
+        System.out.println("Employee Salary Of Month:-" + totalSalary);
     }
 }
